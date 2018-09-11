78f448c415a7a7db171c0c4302f1ac18bb013663
dk-denno <dennisveer27@gmail.com>
2018-09-11 19:18:34 +0300

    Deployment

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 import os
 class Config:
-  
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
     MAIL_PORT = os.environ.get("MAIL_PORT")
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
   
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
@@ -23,6 +21,4 @@ class DevConfig(Config):
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
-
-
 }
