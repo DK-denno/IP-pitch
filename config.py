35f1b35b3778dbc10d012ce2824d7de7fa1b0c7b
dk-denno <dennisveer27@gmail.com>
2018-09-11 18:32:23 +0300

    deployment

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     #  email configurations
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = True
     MAIL_PORT = 587
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
