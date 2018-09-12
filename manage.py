44ca34a75eec487ca5f9598406589594ca56d13f
dk-denno <dennisveer27@gmail.com>
2018-09-12 15:19:37 +0300

    gfdf

--- manage.py
+++ manage.py
@@ -3,8 +3,9 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
 from app.models import User, Posts,Comments
 # Creating app instance
-app = create_app('test')
+# app = create_app('test')
 # app = create_app('development')
+app = create_app('production')
 
 
 manager = Manager(app)
