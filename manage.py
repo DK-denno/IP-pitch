c5d5dd8f68d353119be536d49dcd6170dbf8e927
dk-denno <dennisveer27@gmail.com>
2018-09-12 14:21:42 +0300

    old dog

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
 from app.models import User, Posts,Comments
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
